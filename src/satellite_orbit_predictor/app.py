8ab295999fecfc6498ddc0a32a48c85558d4d5f8
Aaron Margolis <armargolis@gmail.com>
2025-04-03 15:03:27 +0100

    Added constellations
    Added Sentinel, Landsat and BlackSky constellations

--- src/satellite_orbit_predictor/app.py
+++ src/satellite_orbit_predictor/app.py
@@ -8,6 +8,14 @@ ACTIVE_SATS_URL = "http://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMA
 
 constellations = {
     "orbital-sidekick": {"GHOST-1":56195, "GHOST-2":56197, "GHOST-3":56958, "GHOST-4":59133, "GHOST-5":59130},
+    "sentinel": {'SENTINEL-1A': 39634, 'SENTINEL-2A': 40697, 'SENTINEL-3A': 41335, 'SENTINEL-1B': 41456, 
+                  'SENTINEL-2B': 42063, 'SENTINEL-5P': 42969, 'SENTINEL-3B': 43437, 'SENTINEL-6': 46984,
+                  'SENTINEL-2C': 60989, 'SENTINEL-1C': 62261},
+    "blacksky": {'GLOBAL-2': 43812, 'GLOBAL-4': 44499, 'GLOBAL-9': 47971, 'GLOBAL-14': 49469, 'GLOBAL-12': 49772,
+                 'GLOBAL-13': 49773, 'GLOBAL-17': 49949, 'GLOBAL-16': 49950, 'GLOBAL-18': 52196, 'GLOBAL-19': 55982,
+                 'GLOBAL-5': 55983, 'GLOBAL-31': 63027},
+    "landsat": {'LANDSAT 1 (ERTS 1)': 6126, 'LANDSAT 2': 7615, 'LANDSAT 3': 10702, 'LANDSAT 4': 13367,
+                'LANDSAT 5': 14780, 'LANDSAT 7': 25682, 'LANDSAT 8': 39084, 'LANDSAT 9': 49260}
 }
 
 @asynccontextmanager
