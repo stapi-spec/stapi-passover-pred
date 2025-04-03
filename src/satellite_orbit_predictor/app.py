59e2a8016639c0168316787fa9896d5ed2a2ae93
dylan bartels <dylan.bartels@live-eo.com>
2025-04-03 15:25:44 +0100

    Change the reponse to be stapi compliant

--- src/satellite_orbit_predictor/app.py
+++ src/satellite_orbit_predictor/app.py
@@ -1,8 +1,9 @@
+import uvicorn
 from fastapi import FastAPI, Body, Path, Request
 from skyfield.api import load, wgs84, utc
-import uvicorn
 from contextlib import asynccontextmanager
-from stapi_pydantic import OpportunityPayload
+from stapi_pydantic import OpportunityPayload, OpportunityCollection, Opportunity, Link, OpportunityProperties
+from geojson_pydantic.geometries import Point
 
 ACTIVE_SATS_URL = "http://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle"
 
@@ -21,7 +22,7 @@ def opportunities(
     request: Request,
     constellation: str = Path(..., description="Constellation name"),
     opportunity: OpportunityPayload = Body(..., description="Opportunity payload"),
-):
+) -> OpportunityCollection:
     start_time = request.state.ts.from_datetime(opportunity.datetime[0].replace(tzinfo=utc))
     end_time = request.state.ts.from_datetime(opportunity.datetime[1].replace(tzinfo=utc))
     location = wgs84.latlon(opportunity.geometry.coordinates[1], opportunity.geometry.coordinates[0])
@@ -31,8 +32,8 @@ def opportunities(
         satellite = request.state.satellites[satellite_name]
         t, events = satellite.find_events(location, start_time, end_time, altitude_degrees=50.0)
 
-        for ti, event in zip(t, events):
-            if event == 0:
+        for i, (ti, event) in enumerate(zip(t, events)):
+            if event == 1:
                 difference = satellite - location
                 topocentric = difference.at(ti)
 
@@ -40,16 +41,21 @@ def opportunities(
 
                 sat_pos = satellite.at(ti)
                 subpoint = wgs84.subpoint(sat_pos)
+                passes.append(
+                    Opportunity(
+                        geometry=Point(type="Point", coordinates=[opportunity.geometry.coordinates[0], opportunity.geometry.coordinates[1]]),
+                        properties=OpportunityProperties(
+                            satellite=satellite_name,
+                            datetime=(t[i-1].utc_datetime(), t[i+1].utc_datetime()),
+                            altitude=subpoint.elevation.km,
+                            azimuth=az.degrees,
+                            product_id=constellation,
+                        ),
+                        id=satellite_name
+                    )
+                )
 
-                pass_info = {
-                    "satellite": satellite_name,
-                    "datetime": ti.utc_datetime().strftime("%Y-%m-%dT%H:%M:%S"),
-                    "altitude": subpoint.elevation.km,
-                    "azimuth": az.degrees
-                }
-                passes.append(pass_info)
-
-    return passes
+    return OpportunityCollection(features=passes, links=[Link(href=ACTIVE_SATS_URL, rel="via")])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
